2c93083b08fc293dd748086132d2796dcac75793
Ionut Firatoiu <firatoiu.ionut@gmail.com>
2016-02-05 12:52:08 +0200

    routing not working yet + angular store

--- js/main.js
+++ js/main.js
@@ -1,45 +1,112 @@
-/*
-$(document).ready(function() {
-    $('.input-group.date').datetimepicker({
-        format: 'YYYY-MM-DD HH:mm'
-    });
-});
-*/
-
-var app  = angular.module('Sprint6', ['ngRoute']);
-var pages = [
-    {name: 'Home', url: '', ctrl: 'HomeCtrl'},
-    {name: 'Spend', url: 'spend'},
-    {name: 'Receive', url: 'receive'}
-];
-
-app.config(function($locationProvider, $routeProvider) {
-    $locationProvider.html5Mode({
-        enabled: true,
-        requireBase: false
+(function() {
+    $(document).ready(function() {
+        $('.input-group.date').datetimepicker({
+            format: 'YYYY-MM-DD HH:mm'
+        });
     });
 
-    angular.forEach(pages, function(page) {
-        $routeProvider.when('/' + page.url, {
-            templateUrl: 'pages/' + (!page.url ? 'index' : page.url) + '.html',
-            controller: page.ctrl || 'DummyCtrl'
-        })
+    var app  = angular.module('Sprint6', ['ngRoute']);
+    var pages = [
+        {name: 'Home', url: '', ctrl: 'HomeCtrl'},
+        {name: 'Spend', url: 'spend'},
+        {name: 'Receive', url: 'receive'}
+    ];
+
+    app.config(function($locationProvider, $routeProvider) {
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+
+        angular.forEach(pages, function(page) {
+            $routeProvider.when('/' + page.url, {
+                templateUrl: 'pages/' + (!page.url ? 'index' : page.url) + '.html',
+                controller: page.ctrl || 'DummyCtrl'
+            })
+        });
+
+        $routeProvider.otherwise({
+            templateUrl: 'pages/index.html',
+            controller: 'HomeCtrl'
+        });
     });
 
-    $routeProvider.otherwise({
-        templateUrl: 'pages/index.html',
-        controller: 'HomeCtrl'
+    app.controller('MainCtrl', function() {});
+
+    app.controller('DummyCtrl', angular.noop);
+
+    app.controller('HomeCtrl', function($scope, TransactionStore) {
+        $scope.transactions = [];
+
+        TransactionStore.getTransactionsByMonth(moment().format('YYYY-MM')).then(function(items) {
+            $scope.transactions = items;
+        });
     });
-});
 
-app.controller('MainCtrl', function($scope) {});
+    app.factory('TransactionStore', function($http, $q) {
+        return (function() {
+            var URL = 'http://server.godev.ro:8080/api/ionut/transactions';
 
-app.controller('DummyCtrl', angular.noop);
+            var getTransactionsByMonth = function(month) {
+                return $q(function(resolve,reject) {
+                    $http({url: URL + '?month=' + month})
+                        .then(
+                            function(xhr) {
+                                if(xhr.status == 200) {
+                                    resolve(xhr.data);
+                                }else{
+                                    reject();
+                                }
+                            }, reject
+                        );
+                });
+            };
 
-app.controller('HomeCtrl', function($scope, TransactionStore) {
-    $scope.transactions = [];
+            var add = function(data) {
+                return $q(function(resolve,reject) {
+                    $http({
+                        url: URL,
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        data: JSON.stringify(data)
+                    })
+                        .then(
+                            function(xhr) {
+                                if(xhr.status == 201) {
+                                    resolve(xhr.data);
+                                }else{
+                                    reject();
+                                }
+                            }, reject
+                        );
+                });
+            };
 
-    TransactionStore.getTransactionsByMonth(moment().format('YYYY-MM')).then(function(items) {
-        $scope.transactions = items;
+            var del = function(id) {
+                return $q(function(resolve,reject) {
+                    $http({
+                        url: URL + '/' + id,
+                        method: 'DELETE'
+                    })
+                        .then(
+                            function(xhr) {
+                                if(xhr.status == 204) {
+                                    resolve();
+                                }else{
+                                    reject();
+                                }
+                            }, reject
+                        );
+                });
+            };
+
+            return {
+                getTransactionsByMonth: getTransactionsByMonth,
+                add: add,
+                delete: del
+            };
+        })();
     });
-});
+})();
